bb2d308f921f7f23641ee3717caf7443a62dc03a
Your Name <dorian.l.wallace@gmail.com>
2019-03-11 17:06:18 -0400

    first commit

---
+++ src/main/java/com/satxvitalrecords/SatxvitalrecordsApplication.java
@@ -0,0 +1,13 @@
+package com.satxvitalrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatxvitalrecordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatxvitalrecordsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/satxvitalrecords/models/User.java
@@ -0,0 +1,4 @@
+package com.satxvitalrecords.models;
+
+public class User {
+}
