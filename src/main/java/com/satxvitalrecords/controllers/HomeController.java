5e42286a98d05a957c7f50c0f5790fcb8c6bb51d
Sarah Barron <s.barron2338@gmail.com>
2019-03-25 22:44:08 -0500

    eod commit

--- src/main/java/com/satxvitalrecords/controllers/HomeController.java
+++ src/main/java/com/satxvitalrecords/controllers/HomeController.java
@@ -93,6 +93,24 @@ public class HomeController {
     public String viewAllApps(@PathVariable long id, Model model) {
         Application app = appDao.findOne(id);
         Record record = recordDao.findOne(id);
+
+        int inprogress = numberOfApps(appDao.findAll(), "In Progress");
+        int need_docs = numberOfApps(appDao.findAll(), "Need Uploads");
+        int pending_review = numberOfApps(appDao.findAll(), "Pending Review");
+        int approved = numberOfApps(appDao.findAll(), "Approved");
+        int mailed = numberOfApps(appDao.findAll(), "Mailed");
+        int pickedup = numberOfApps(appDao.findAll(), "Picked-up");
+        System.out.println(inprogress);
+
+        model.addAttribute("apps", appDao.findAll());
+        model.addAttribute("inprogress", inprogress);
+        model.addAttribute("pendingreview", pending_review);
+        model.addAttribute("needdocs", need_docs);
+        model.addAttribute("approved", approved);
+        model.addAttribute("mailed", mailed);
+        model.addAttribute("pickedup", pickedup);
+
+
         model.addAttribute("app", app);
         model.addAttribute("record", record);
         return "app-view";
@@ -100,16 +118,19 @@ public class HomeController {
 
     @PostMapping("/app-view/{id}")
     public String leaveComment(@PathVariable long id, @RequestParam(name = "comments") String comments) {
+        Application app = appDao.findOne(id);
 //        Date date = new Date();
 //        DateFormat dateFormat = new SimpleDateFormat("YY/MM/DD hh:mm:ss");
 //        dateFormat.format(date);
-        Application app = appDao.findOne(id);
         app.setComments(comments);
 //        Date date = new Date();
 //        String strDateFormat = "YY/MM/DD hh:mm:ss";
 //        DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
 //       dateFormat.format(date);
 //        app.setComment_dateTime(date);
+
+
+
         appDao.save(app);
         return "redirect: /app-index";
     }
